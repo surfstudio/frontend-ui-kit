325d4508ac311ee629a9c7abac1860c132a5b258
Mikhail Solomatin <newmihei@gmail.com>
2025-02-14 11:34:08 +0300

    поправил пути, поправил конфиги

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,27 +1,24 @@
-import type { Preview } from "@storybook/react";
-import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
-  parameters: {
-    viewport: {
-      viewports: INITIAL_VIEWPORTS,
-      defaultViewport: "iphone12promax",
+    parameters: {
+        viewport: {
+            viewports: INITIAL_VIEWPORTS,
+        },
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        options: {
+            storySort: (a, b) => {
+                return a.title === b.title ? 0 : a.id.localeCompare(b.id, { numeric: true });
+            },
+        },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    options: {
-      storySort: (a, b) => {
-        return a.title === b.title
-          ? 0
-          : a.id.localeCompare(b.id, { numeric: true });
-      },
-    },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
 };
 
 export default preview;
--- packages/ui-library/.storybook/main.ts
+++
@@ -1,8 +0,0 @@
-import commonConfigs from "../../../.storybook/main";
-
-const config = {
-  ...commonConfigs,
-  stories: ["../src/**/*..mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-};
-
-export default config;
